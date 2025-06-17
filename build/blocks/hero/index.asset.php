bdaf6db19d69abde336886ec492350bfa20cf081
fizikas77 <fizikas77@gmail.com>
2025-06-17 16:26:45 +0300

    update version update short-article add github action in workflows

--- build/blocks/hero/index.asset.php
+++ build/blocks/hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2585a7d9f0031a90f913');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2585a7d9f0031a90f915');
--- build/blocks/short-article/index.asset.php
+++ build/blocks/short-article/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a7c8c8ceac66ff521dca');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a7c8c8ceac66ff521dc1');
